85b90b143e2683eee07c83a7fde019de4a5fdf3d
leeyh-zh <leeyh_zh@163.com>
2018-05-25 17:28:24 +0800

    Create HourForecastsWeatherData.java

---
+++ app/src/main/java/com/opweather/bean/HourForecastsWeatherData.java
@@ -0,0 +1,47 @@
+package com.opweather.bean;
+
+public class HourForecastsWeatherData {
+    private String hourText;
+    private String temperature;
+    private int weatherIconId;
+    private int weatherId;
+
+    public HourForecastsWeatherData(String hourText, String temperature, int weatherIconId, int weatherId) {
+        this.hourText = hourText;
+        this.temperature = temperature;
+        this.weatherIconId = weatherIconId;
+        this.weatherId = weatherId;
+    }
+
+    public String getHourText() {
+        return hourText;
+    }
+
+    public void setHourText(String hourText) {
+        this.hourText = hourText;
+    }
+
+    public String getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(String temperature) {
+        this.temperature = temperature;
+    }
+
+    public int getWeatherIconId() {
+        return weatherIconId;
+    }
+
+    public void setWeatherIconId(int weatherIconId) {
+        this.weatherIconId = weatherIconId;
+    }
+
+    public int getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(int weatherId) {
+        this.weatherId = weatherId;
+    }
+}
