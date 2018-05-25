8a5e27f245b7de308c94ce44effdbd7cf7db6b29
leeyh-zh <leeyh_zh@163.com>
2018-05-25 17:27:53 +0800

    Create HourForecastsWeather.java

---
+++ app/src/main/java/com/opweather/bean/HourForecastsWeather.java
@@ -0,0 +1,27 @@
+package com.opweather.bean;
+
+import android.content.Context;
+
+import com.opweather.opapi.AbstractWeather;
+import com.opweather.opapi.Temperature;
+
+import java.util.Date;
+
+public abstract class HourForecastsWeather extends AbstractWeather {
+
+    public abstract Temperature getTemperature();
+
+    public abstract Date getTime();
+
+    public abstract int getWeatherId();
+
+    public abstract String getWeatherText(Context context);
+
+    public HourForecastsWeather(String areaCode, String areaName, String dataSource) {
+        super(areaCode, areaName, dataSource);
+    }
+
+    public String getWeatherName() {
+        return "Hour Forecasts Weather";
+    }
+}
