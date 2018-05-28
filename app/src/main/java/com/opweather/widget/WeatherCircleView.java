3ce49a6a8b02c1510bcff3632df2f4659d4c34e7
leeyh-zh <leeyh_zh@163.com>
2018-05-28 17:06:13 +0800

    Create WeatherCircleView.java

---
+++ app/src/main/java/com/opweather/widget/WeatherCircleView.java
@@ -0,0 +1,13 @@
+package com.opweather.widget;
+
+public class WeatherCircleView {
+    private static final float ALL_ARC = 360.0f;
+    public static final int ARC_DIN = 12;
+    public static final int DIN_ANGEL = 15;
+    private static final float END_ARC = 360.0f;
+    public static final int START_ANGEL_0 = 0;
+    public static final int START_ANGEL_180 = 180;
+    public static final int START_ANGEL_270 = 270;
+    public static final int START_ANGEL_90 = 90;
+    private static final float START_ARC = 135.0f;
+}
