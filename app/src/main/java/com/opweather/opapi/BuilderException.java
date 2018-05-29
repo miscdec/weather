54c158241f415e075cba21f12602f6ff154338d0
leeyh-zh <leeyh_zh@163.com>
2018-05-29 14:58:50 +0800

    Create BuilderException.java

---
+++ app/src/main/java/com/opweather/opapi/BuilderException.java
@@ -0,0 +1,9 @@
+package com.opweather.opapi;
+
+public class BuilderException extends ParseException {
+    private static final long serialVersionUID = -4617647961185389355L;
+
+    public BuilderException(String detailMessage) {
+        super(detailMessage);
+    }
+}
