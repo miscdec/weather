659ed5e790b2dfed71bd69baa0e95212ba21cbce
leeyh-zh <leeyh_zh@163.com>
2018-05-29 15:02:25 +0800

    Create WeatherException.java

---
+++ app/src/main/java/com/opweather/opapi/WeatherException.java
@@ -0,0 +1,17 @@
+package com.opweather.opapi;
+
+public class WeatherException extends Exception {
+    private static final long serialVersionUID = -6316970406795805454L;
+
+    public WeatherException(String detailMessage) {
+        super(detailMessage);
+    }
+
+    public WeatherException(Throwable throwable) {
+        super(throwable);
+    }
+
+    public WeatherException(String detailMessage, Throwable throwable) {
+        super(detailMessage, throwable);
+    }
+}
