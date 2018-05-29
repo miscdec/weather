043de1b93dd3ebcd3d745357dfd1939fe7f39313
leeyh-zh <leeyh_zh@163.com>
2018-05-29 15:00:39 +0800

    Create ResponseParser.java

---
+++ app/src/main/java/com/opweather/opapi/ResponseParser.java
@@ -0,0 +1,21 @@
+package com.opweather.opapi;
+
+public interface ResponseParser {
+
+    interface WeatherBuilder {
+        RootWeather build() throws BuilderException;
+    }
+
+    RootWeather parseAlarm(byte[] bArr) throws ParseException;
+
+    RootWeather parseAqi(byte[] bArr) throws ParseException;
+
+    RootWeather parseCurrent(byte[] bArr) throws ParseException;
+
+    RootWeather parseDailyForecasts(byte[] bArr) throws ParseException;
+
+    RootWeather parseHourForecasts(byte[] bArr) throws ParseException;
+
+    RootWeather parseLifeIndex(byte[] bArr) throws ParseException;
+}
+
